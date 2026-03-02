63f71e6f6f4bb8abce3f7e1e8e5edd12b4e807b6
Grant Stephens <gstephens@fastly.com>
2026-03-02 20:24:45 +0000

    feat: initialize theme on app start

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { render } from 'preact'
 import { App } from './app'
+import { initTheme } from './lib/theme'
 import './style.css'
 
+initTheme()
 render(<App />, document.getElementById('app')!)
